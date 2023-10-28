9abafbfdc1bd10d82bb7f6260fc7b4a61b840309
devFresher <fresher.dev01@gmail.com>
2023-10-28 12:56:01 +0100

    fix

--- src/modules/category/category.validate.ts
+++ src/modules/category/category.validate.ts
@@ -28,7 +28,7 @@ export default class CategoryValidator {
 
 	static markShortlist(req: Request): ValidationResult {
 		const validationSchema = Joi.object({
-			stage: Joi.number().valid('1,2,3').label('Stage Number'),
+			stage: Joi.number().valid(1, 2, 3).label('Stage Number'),
 		});
 
 		return validationSchema.validate(req.body, options);
